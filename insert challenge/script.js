cd5327ecb253a90f09a5db8ad3d0dcf6e2968371
Eren İşgören <108978390+erenoske@users.noreply.github.com>
2023-10-21 01:35:34 +0300

    Add files via upload

---
+++ insert challenge/script.js
@@ -0,0 +1,31 @@
+function createItem(item) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(item));
+
+    li.className = 'item';
+    const span = document.createElement('span');
+    span.appendChild(document.createTextNode(' x'));   
+
+    li.appendChild(span);
+
+    document.querySelector('ul').appendChild(li);
+}
+
+function createItemAfter(item) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(item));
+
+    li.className = 'item';
+    const span = document.createElement('span');
+    span.appendChild(document.createTextNode(' x'));   
+
+    li.appendChild(span);
+
+    const firstItem = document.querySelector('ul li:first-child');
+
+
+    document.querySelector('ul').insertBefore(li, firstItem);
+}
+
+createItem('item 4');
+createItemAfter('item 5');
